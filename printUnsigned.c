e0c3d194b3f1bd1805bf5fa83bd12c4d90199dc7
kalab amanuel <kamanuel61@gmail.com>
2023-09-21 05:21:44 -0700

    dhfslkv

---
+++ printUnsigned.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * printUnsigned - prints int
+ * @args: argument
+ * Return: number of char
+ */
+
+int printUnsigned(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		putChar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			putChar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	putChar(last + '0');
+	return (i);
+}
