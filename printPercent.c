f01232f9714372adf0eedebe7376f8d8db937b63
kalab amanuel <kamanuel61@gmail.com>
2023-09-21 05:13:17 -0700

    fhj,sfd

---
+++ printPercent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * printPercent - a functiont to print % symbol  by - ASCII code 37
+ * Return: returns 1 upon success.
+ */
+
+int printPercent(void)
+{
+	putChar(37);
+	return (1);
+}
