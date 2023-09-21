52ed0180b5ba9ab3a30d169886d5ae5594c4dff2
kalab amanuel <kamanuel61@gmail.com>
2023-09-21 05:25:02 -0700

    kkjgwkkvje

---
+++ putChar.c
@@ -0,0 +1,14 @@
+i#include "main.h"
+
+/**
+ * putChar - writes the character c to stdout.
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int putChar(char c)
+{
+	return (write(1, &c, 1));
+}
